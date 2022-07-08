ef541483094eed96a0d5a412349d34c24742f598
Valentina Macek <vm5180@student.uni-lj.si>
2022-07-08 12:20:15 +0200

    zacetek z operacijami na matrikah

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
-class Matrike:
+import numpy as np
+class Matrika:
     def __init__(self, matrika):
-        self.matrika = matrika
+        self.matrika = np.array(matrika)
         self.st_vrstic=len(matrika)
         self.st_stolpcev=len(matrika[0])
+    def transponiraj(self):
+        return Matrika(self.matrika.transpose())
+    def __add__(self, other):
+        if self.st_stolpcev == other.st_stolpcev and self.st_vrstic == other.st_vrstic:
+            return Matrika(self.matrika + other.matrika)
+        
