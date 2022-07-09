38bafa21682e48eac8b8e0362901da4655071725
Valentina Macek <vm5180@student.uni-lj.si>
2022-07-09 19:01:44 +0200

    dodala množenje matrik

--- model.py
+++ model.py
@@ -9,4 +9,22 @@ class Matrika:
     def __add__(self, other):
         if self.st_stolpcev == other.st_stolpcev and self.st_vrstic == other.st_vrstic:
             return Matrika(self.matrika + other.matrika)
-        
+        else:
+            print("Matriki morata biti iste velikosti")
+    def __mul__(self, other):
+        if self.st_stolpcev == other.st_vrstic:
+            nova=[]
+            othert=other.transponiraj()
+            for vrstica in self.matrika:
+                novavrst=[]
+                for stolpec in othert.matrika:
+                    vsota=0
+                    for element, oelement in zip(vrstica, stolpec): 
+                        vsota+=element*oelement
+                    novavrst.append(vsota)
+                nova.append(novavrst)
+            return Matrika(nova)
+        else:
+            print("Prva matrika mora imeti toliko stolpcev kot ima druga matrika vrstic")
+     
+   
