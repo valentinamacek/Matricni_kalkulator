ae0fdf53c2a7c7365dab293ec64738f443707736
Valentina Macek <vm5180@student.uni-lj.si>
2022-07-13 11:35:33 +0200

    nadaljevanje tekstovnega vmesnika

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -28,20 +28,40 @@ def izberi_moznost(moznosti):
             print(f"Vnesti morate število med 1 in {len(moznosti)}.")
 
 def transponiraj_matriko(seznam):
-
     transponirana= seznam.transponiraj()
-    niz=print(transponirana.matrika)
-    return niz
+    stanje.dodaj_matriko(transponirana)
+    print(transponirana.matrika)
 
 def determinanta_od_matrike(seznam):
     det=seznam.det()
     rezultat= f"{det}"
     print(rezultat)
-    return rezultat
-   
 
+def sestej(matrika1, matrika2):
+    assert isinstance(matrika1, Matrika) and isinstance(matrika2, Matrika)
+    vsota= matrika1 + matrika2
+    stanje.dodaj_matriko(vsota)
+    print(vsota.matrika)
 
-def ponudi_moznosti(seznam):
+def zmnozi(matrika1, matrika2):
+    assert isinstance(matrika1, Matrika) and isinstance(matrika2, Matrika)
+    produkt=matrika1*matrika2
+    stanje.dodaj_matriko(produkt)
+    print(produkt.matrika)
+
+def prikaz_Matrike(seznam):
+    niz = f"{seznam.matrika}\n stevilo vrstic:{seznam.st_vrstic}\n stevilo stolpcev:{seznam.st_stolpcev}"
+    return niz
+
+def izpisi_trenutno_stanje():
+    for matrika in stanje.matrike:
+        print(prikaz_Matrike(matrika))
+    if not stanje.matrike:
+        print(
+            "Trenutno nimate še nobene matrike"
+        )
+
+def moznosti_posz(seznam):
     izbrana_operacija= izberi_moznost(
         [
             (transponiraj_matriko, "transponiraj matriko"),
@@ -52,6 +72,15 @@ def ponudi_moznosti(seznam):
     )
     izbrana_operacija(seznam)
 
+def moznosti_dveh(seznam1, seznam2):
+    izbrana_operacija = izberi_moznost(
+        [
+            (sestej, "seštej matriki"),
+            (zmnozi, "zmnoži matriki"),
+        ]
+    )
+    izbrana_operacija(seznam1, seznam2)
+
 def je_matrika_s_stevili(niz):
     assert isinstance(niz, str)
     for znak in niz:
@@ -59,7 +88,14 @@ def je_matrika_s_stevili(niz):
             if znak!=';' and znak!=',' and znak!=' ':
                 return False
     return True
-
+def izberi_matriko(stanje):
+    print("Izberi matriko:")
+    return izberi_moznost(
+        [
+            (matrika, prikaz_Matrike(matrika))
+            for matrika in stanje.matrike
+        ]
+    )
 def dodaj_matriko():
         print("Vnesi matriko")
         vnos =input()
@@ -67,9 +103,32 @@ def dodaj_matriko():
             seznam=np.matrix(vnos)
             matrika=Matrika(seznam)
             stanje.dodaj_matriko(matrika)
-            ponudi_moznosti(matrika)
+            moznosti_posz(matrika)
         else:
             print("Vnesi stevila")
 
+def zacetni_pozdrav():
+    print("Pozdravljeni v matričnem kalkulatorju")
 
+def ponudi_moznosti():
+    if not stanje.matrike:
+        dodaj_matriko()
+    elif len(stanje.matrike)==2:
+        matrika1=stanje.matrike[0]
+        matrika2=stanje.matrike[1]
+        moznosti_dveh(matrika1, matrika2)
+    elif len(stanje.matrike)==1:
+        matrika=stanje.matrike[0]
+        moznosti_posz(matrika)
+    else:
+        matrika1 = izberi_matriko(stanje)
+        matrika2 = izberi_matriko(stanje)
+        moznosti_dveh(matrika1, matrika2)
 
+def tekstovni_vmesnik():
+    zacetni_pozdrav()
+    while True:
+        izpisi_trenutno_stanje()
+        ponudi_moznosti()
+
+tekstovni_vmesnik()
