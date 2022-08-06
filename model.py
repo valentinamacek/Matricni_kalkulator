cd043a551620d6b41ebd52d1378d89bd3407fb9b
Valentina Macek <vm5180@student.uni-lj.si>
2022-08-06 18:11:41 +0200

    formatiranje dokumentov

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class Stanje:
         with open(ime_datoteke) as dat:
             slovar = json.load(dat)
             return Stanje.iz_slovarja(slovar)
-  
+
 
 class Matrika:
     def __init__(self, matrika):
@@ -53,7 +53,7 @@ class Matrika:
 
     def transponiraj(self):
         return Matrika(self.matrika.transpose())
-    
+
     def __eq__(self, other):
         return np.array_equal(self.matrika, other.matrika)
 
@@ -61,42 +61,42 @@ class Matrika:
         if self.st_stolpcev == other.st_stolpcev and self.st_vrstic == other.st_vrstic:
             return Matrika(self.matrika + other.matrika)
         else:
-            return {"vsota" :"Matriki morata biti iste velikosti"}
+            return {"vsota": "Matriki morata biti iste velikosti"}
 
     def __mul__(self, other):
         if self.st_stolpcev == other.st_vrstic:
             nova = []
-            othert = other.transponiraj()
+            other_transponiran = other.transponiraj()
             for vrstica in self.matrika:
                 novavrst = []
-                for stolpec in othert.matrika:
+                for stolpec in other_transponiran.matrika:
                     vsota = 0
-                    for element, oelement in zip(vrstica, stolpec):
-                        vsota += element*oelement
+                    for element_self, element_other in zip(vrstica, stolpec):
+                        vsota += element_self * element_other
                     novavrst.append(vsota)
                 nova.append(novavrst)
             return Matrika(nova)
         else:
-            return {"produkt" : "Prva matrika mora imeti toliko stolpcev kot ima druga matrika vrstic" }
+            return {"produkt": "Prva matrika mora imeti toliko stolpcev kot ima druga matrika vrstic"}
 
     def potenciraj(self, n):
         try:
             float(n)
-            if not float(n).is_integer() or float(n)<=0:
-                return  {"potenciraj":"Vnesi naravno število"}
-            g=float(n)
-            c=int(g)
+            if not float(n).is_integer() or float(n) <= 0:
+                return {"potenciraj": "Vnesi naravno število"}
+            g = float(n)
+            c = int(g)
             if self.st_stolpcev == self.st_vrstic:
-                k = c-1
+                k = c - 1
                 potencirana = self
                 while k != 0:
                     potencirana = potencirana * self
                     k -= 1
                 return potencirana
             else:
-                return {"potenciraj":"Matrika mora biti kvadratna"}
+                return {"potenciraj": "Matrika mora biti kvadratna"}
         except ValueError:
-            return {"potenciraj":"Vnesi naravno število"}
+            return {"potenciraj": "Vnesi naravno število"}
 
     def mnozenje_s_skalar(self, skalar):
         try:
@@ -104,7 +104,7 @@ class Matrika:
         except ValueError:
             try:
                 fractions.Fraction(skalar)
-                skalar=float(fractions.Fraction(skalar))
+                skalar = float(fractions.Fraction(skalar))
             except ValueError:
                 return {"skalar": "Vnesi realno število"}
         nova = []
@@ -117,6 +117,7 @@ class Matrika:
         return Matrika(nova)
 
     def det(self):
+        # izračuna determinanto matrike na rekurziven način z razvojem po prvi vrstici
         if self.st_stolpcev == self.st_vrstic:
             n = self.st_stolpcev
             if n == 1:
@@ -127,28 +128,29 @@ class Matrika:
                 return vsota
             else:
                 vsota = 0
-                for i, element in enumerate(self.matrika[0]):
-                    brezprve = Matrika(self.matrika[1:])
-                    brezprvet = brezprve.transponiraj()
-                    brezistolpca = Matrika(np.delete(brezprvet.matrika, i, 0))
-                    if i % 2 == 0:
-                        vsota += element*brezistolpca.det()
+                for j_stolpec, element in enumerate(self.matrika[0]):
+                    brezprve_vrstice = Matrika(self.matrika[1:])
+                    brez_j_stolpca = Matrika(
+                        np.delete(brezprve_vrstice.matrika, j_stolpec, 1))
+                    if j_stolpec % 2 == 0:
+                        vsota += element*brez_j_stolpca.det()
                     else:
-                        vsota -= element*brezistolpca.det()
+                        vsota -= element*brez_j_stolpca.det()
                 return vsota
         else:
-            return {"det" :"Matrika mora biti kvadratna"}
+            return {"det": "Matrika mora biti kvadratna"}
 
     def prirejenka(self):
         if self.st_stolpcev == self.st_vrstic:
             nova = []
-            for i in range(self.st_vrstic):
+            for i_vrstica in range(self.st_vrstic):
                 novav = []
-                for j in range(self.st_stolpcev):
-                    k = i+j
-                    brez_i_vrst = Matrika(np.delete(self.matrika, i, 0))
+                for j_stolpec in range(self.st_stolpcev):
+                    k = i_vrstica+j_stolpec
+                    brez_i_vrst = Matrika(
+                        np.delete(self.matrika, i_vrstica, 0))
                     brez_j_stolpca = Matrika(
-                        np.delete(brez_i_vrst.matrika, j, 1))
+                        np.delete(brez_i_vrst.matrika, j_stolpec, 1))
                     if k % 2 == 0:
                         poddet = brez_j_stolpca.det()
                     else:
@@ -157,7 +159,7 @@ class Matrika:
                 nova.append(novav)
             return Matrika(nova)
         else:
-            return {"prirejenka" : "Matrika mora biti kvadratna"}
+            return {"prirejenka": "Matrika mora biti kvadratna"}
 
     def inverz(self):
         if self.st_stolpcev == self.st_vrstic:
@@ -170,7 +172,7 @@ class Matrika:
             else:
                 return {"inverz": "Matrika ni obrnljiva"}
         else:
-           return {"inverz": "Matrika ni obrnljiva"}
+            return {"inverz": "Matrika ni obrnljiva"}
 
     def sled(self):
         if self.st_stolpcev == self.st_vrstic:
@@ -179,7 +181,7 @@ class Matrika:
                 sled += self.matrika[i][i]
             return sled
         else:
-            return {"sled" : "Matrika mora biti kvadratna"}
+            return {"sled": "Matrika mora biti kvadratna"}
 
     def v_slovar(self):
         seznam = self.matrika.tolist()
@@ -188,44 +190,44 @@ class Matrika:
             "st_vrstic": self.st_vrstic,
             "st_stolpcev": self.st_stolpcev,
         }
+
     @staticmethod
     def iz_slovarja(slovar):
         return Matrika(slovar["matrika"])
-    
+
     def spremeni_obliko_matrike(self):
-        nova=[]
+        nova = []
         for vrstica in self.matrika:
-            novav=[]
+            novav = []
             for element in vrstica:
-                nov= str(fractions.Fraction(element).limit_denominator())
+                nov = str(fractions.Fraction(element).limit_denominator())
                 novav.append(nov)
             nova.append(novav)
         return nova
-    
+
     @staticmethod
     def spremeni_v_matriko(niz):
-        if niz=="":
+        if niz == "":
             return {"matrika": "Še enkrat preberi navodila"}
         else:
-           vrstice=niz.split(";")
-           seznam=[]
-           prvavrstica=vrstice[0].split()
-           st_stolpcev=len(prvavrstica)
-           for vrstica in vrstice:
-                seznamv=[]
-                elementi=vrstica.split()
+            vrstice = niz.split(";")
+            seznam = []
+            prvavrstica = vrstice[0].split()
+            st_stolpcev = len(prvavrstica)
+            for vrstica in vrstice:
+                seznamv = []
+                elementi = vrstica.split()
                 if len(elementi) != st_stolpcev:
                     return {"matrika": "To ni matrika"}
                 for element in elementi:
-                      try:
+                    try:
                         float(element)
-                      except ValueError:
+                    except ValueError:
                         try:
                             fractions.Fraction(element)
-                            element=float(fractions.Fraction(element))
+                            element = float(fractions.Fraction(element))
                         except ValueError:
-                          return {"matrika": "Še enkrat preberi navodila"}
-                      seznamv.append(element)
+                            return {"matrika": "Še enkrat preberi navodila"}
+                    seznamv.append(element)
                 seznam.append(seznamv)
-           return Matrika(seznam)
-        
+            return Matrika(seznam)
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,16 +1,19 @@
 from ssl import OP_ALL
 import bottle
 from model import Stanje, Matrika
-import numpy as np
+
 
 SIFRIRNI_KLJUC = "To je skrivnost"
-OPERACIJE=["transponiraj", "prirejenka", "inverz", "det", "sled"]
+OPERACIJE = ["transponiraj", "prirejenka", "inverz", "determinanta", "sled"]
+
 
 def ime_uporabnikove_datoteke(uporabnisko_ime):
     return f"stanja_uporabnikov/{uporabnisko_ime}.json"
 
+
 def stanje_trenutnega_uporabnika():
-    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime", secret=SIFRIRNI_KLJUC)
+    uporabnisko_ime = bottle.request.get_cookie(
+        "uporabnisko_ime", secret=SIFRIRNI_KLJUC)
     if uporabnisko_ime == None:
         bottle.redirect("/prijava/")
     else:
@@ -23,10 +26,13 @@ def stanje_trenutnega_uporabnika():
         stanje.shrani_v_datoteko(ime_datoteke)
     return stanje
 
+
 def shrani_stanje_trenutnega_uporabnika(stanje):
-    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime", secret=SIFRIRNI_KLJUC)
+    uporabnisko_ime = bottle.request.get_cookie(
+        "uporabnisko_ime", secret=SIFRIRNI_KLJUC)
     ime_datoteke = ime_uporabnikove_datoteke(uporabnisko_ime)
     stanje.shrani_v_datoteko(ime_datoteke)
+
 
 @bottle.get("/prijava/")
 def prijava_get():
@@ -34,13 +40,15 @@ def prijava_get():
         "prijava.tpl"
     )
 
+
 @bottle.post("/prijava/")
 def prijava_post():
     uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
     geslo = bottle.request.forms.getunicode("geslo")
-    bottle.response.set_cookie("uporabnisko_ime", uporabnisko_ime, path="/", secret=SIFRIRNI_KLJUC)
+    bottle.response.set_cookie(
+        "uporabnisko_ime", uporabnisko_ime, path="/", secret=SIFRIRNI_KLJUC)
     bottle.redirect("/")
-    
+
 
 @bottle.post("/odjava/")
 def odjava_post():
@@ -55,43 +63,49 @@ def staticno_oblikovanje(file):
 
 @bottle.get("/dodaj-matriko/")
 def dodaj_matriko():
-    return  bottle.template("dodaj_matriko.tpl", napake={})
+    return bottle.template("dodaj_matriko.tpl", napake={})
+
 
 @bottle.get("/odstrani-matriko/")
 def odstrani_matriko():
-    stanje=stanje_trenutnega_uporabnika()
-    return  bottle.template("odstrani_matriko.tpl", matrike=stanje.matrike, napake={})
+    stanje = stanje_trenutnega_uporabnika()
+    return bottle.template("odstrani_matriko.tpl", matrike=stanje.matrike, napake={})
+
 
 @bottle.get("/")
 def zacetna_stran():
-    stanje=stanje_trenutnega_uporabnika()
+    stanje = stanje_trenutnega_uporabnika()
     return bottle.template(
         "zacetna_stran.tpl",
         matrike=stanje.matrike, operacije=OPERACIJE)
 
+
 @bottle.post("/izberi_stevilo/")
 def izbira_st_matrik():
-    stevilo=bottle.request.forms["izbira"]
-    if stevilo=="ena":
+    stevilo = bottle.request.forms["izbira"]
+    if stevilo == "ena":
         bottle.redirect("/operacije-ena/")
     else:
         bottle.redirect("/operacije-dve/")
 
+
 @bottle.get("/operacije-ena/")
 def operacije_ena_prikaz():
-    stanje=stanje_trenutnega_uporabnika()
+    stanje = stanje_trenutnega_uporabnika()
     return bottle.template("operacije_ena.tpl", matrike=stanje.matrike, operacije=OPERACIJE)
+
 
 @bottle.get("/operacije-dve/")
 def operacije_dve_prikaz():
-    stanje=stanje_trenutnega_uporabnika()
+    stanje = stanje_trenutnega_uporabnika()
     return bottle.template("operacije_dve.tpl", matrike=stanje.matrike, operacije=OPERACIJE)
+
 
 @bottle.post("/dodaj-matriko/")
 def dodaj_matriko_post():
-    stanje=stanje_trenutnega_uporabnika()
-    elementi=bottle.request.forms["matrika"]
-    pretvorba=Matrika.spremeni_v_matriko(elementi)
+    stanje = stanje_trenutnega_uporabnika()
+    elementi = bottle.request.forms["matrika"]
+    pretvorba = Matrika.spremeni_v_matriko(elementi)
     if isinstance(pretvorba, Matrika):
         napake = stanje.preveri_podatke_nove_matrike(pretvorba)
         if napake:
@@ -103,123 +117,135 @@ def dodaj_matriko_post():
     else:
         return bottle.template("dodaj_matriko.tpl", napake=pretvorba)
 
+
 @bottle.post("/odstrani-matriko/")
 def odstrani_matriko_post():
-    stanje=stanje_trenutnega_uporabnika()
-    izbrane=[]
+    stanje = stanje_trenutnega_uporabnika()
+    izbrane = []
     for id_matrike in range(len(stanje.matrike)):
-        if bottle.request.forms.get(f"{id_matrike}")!= None:
-           izbrane.append(id_matrike)
-    if izbrane==[]:
-        napake = {"izbor" : "Izberi vsaj eno matriko , ki jo želiš izbrisati, ali pritisni prekliči"}
-        return  bottle.template("odstrani_matriko.tpl", matrike=stanje.matrike, napake=napake)
+        if bottle.request.forms.get(f"{id_matrike}") != None:
+            izbrane.append(id_matrike)
+    if not izbrane:
+        napake = {
+            "izbor": "Izberi vsaj eno matriko , ki jo želiš izbrisati, ali pritisni prekliči"}
+        return bottle.template("odstrani_matriko.tpl", matrike=stanje.matrike, napake=napake)
     else:
         for id_izbrane_matrike in izbrane[::-1]:
             stanje.odstrani_matriko(stanje.matrike[id_izbrane_matrike])
         shrani_stanje_trenutnega_uporabnika(stanje)
         bottle.redirect("/")
-        
-@bottle.post("/operacija-dve/<stanje_r:int>/<id_matrike1>/<id_matrike2>/")
-def operacija_dve(stanje_r, id_matrike1, id_matrike2):
-    stanje=stanje_trenutnega_uporabnika()
-    if id_matrike1=='n' and id_matrike2=='n':
-        id_matrike1 = int(bottle.request.forms["matrika1"])
-        id_matrike2 =int(bottle.request.forms["matrika2"])
-    napake={}
-    matrika1 = stanje.matrike[int(id_matrike1)]
-    matrika2 = stanje.matrike[int(id_matrike2)]
-    vsota = matrika1 + matrika2
-    if isinstance(vsota, Matrika)==False:
-       napake.update(vsota)
-    produkt = matrika1 * matrika2
-    if isinstance(produkt, Matrika)== False:
-       napake.update(produkt)
-    if stanje_r==1 and isinstance(vsota, Matrika):
-        napaka = stanje.preveri_podatke_nove_matrike(vsota)
-        if not napaka:
-            stanje.dodaj_matriko(vsota)
-            shrani_stanje_trenutnega_uporabnika(stanje)
-    elif stanje_r==2 and isinstance(produkt, Matrika):
-        napaka = stanje.preveri_podatke_nove_matrike(produkt)
-        if not napaka:
-            stanje.dodaj_matriko(produkt)
-            shrani_stanje_trenutnega_uporabnika(stanje)
-    return bottle.template("rezultat_dveh.tpl", matrika1=matrika1, matrika2=matrika2, vsota=vsota, produkt=produkt,
-        id_matrike1=id_matrike1,id_matrike2=id_matrike2, matrike=stanje.matrike, operacije=OPERACIJE, napake=napake)
-    
+
 
 @bottle.post("/operacija-ena/<stanje_r:int>/<id_matrike>/<stopnja>/<skalar>/")
 def operacija_ena(stanje_r, id_matrike, stopnja, skalar):
-    stanje=stanje_trenutnega_uporabnika()
-    if id_matrike=='n':
-       id_matrike = int(bottle.request.forms["matrike"])
-    napake={}
+    # izracuna rezultate operacij glede na izbrano matriko
+    # uporabljena je v vecih formsih, kaj spremeni je odvisno glede na to kaksne so vrednosti spremenljivk v url
+    # ko izbiramo id_matrike, skalar ali stopnjo imajo vrednost n, potem pa se nastavijo na vrednost, ki je bila izbrana
+    stanje = stanje_trenutnega_uporabnika()
+    if id_matrike == 'n':
+        id_matrike = int(bottle.request.forms["matrike"])
+    napake = {}
     matrika = stanje.matrike[int(id_matrike)]
     transponirana = matrika.transponiraj()
     prirejenka = matrika.prirejenka()
-    if isinstance(prirejenka, Matrika)==False:
+    if not isinstance(prirejenka, Matrika):
         napake.update(prirejenka)
     inverz = matrika.inverz()
-    if isinstance(inverz, Matrika)==False:
+    if not isinstance(inverz, Matrika):
         napake.update(inverz)
     determinanta = matrika.det()
-    if isinstance(determinanta, float)==False:
+    if not isinstance(determinanta, float):
         napake.update(determinanta)
     sled = matrika.sled()
-    if isinstance(sled, float)==False:
+    if not isinstance(sled, float):
         napake.update(sled)
-    if stanje_r==1:
+    # stanje_r je primarno 0, pri kliku na gumb Shrani rezultat v seznam mojih matrik,
+    # pa je njegova vrednost odvisna od tega, kateri rezultat zelimo shraniti
+    if stanje_r == 1:
         napaka = stanje.preveri_podatke_nove_matrike(transponirana)
         if not napaka:
             stanje.dodaj_matriko(transponirana)
             shrani_stanje_trenutnega_uporabnika(stanje)
     else:
-         stanje=stanje_trenutnega_uporabnika()
-    if stanje_r==2 and isinstance(prirejenka, Matrika):
+        stanje = stanje_trenutnega_uporabnika()
+    if stanje_r == 2 and isinstance(prirejenka, Matrika):
         napaka = stanje.preveri_podatke_nove_matrike(prirejenka)
         if not napaka:
             stanje.dodaj_matriko(prirejenka)
             shrani_stanje_trenutnega_uporabnika(stanje)
-    elif stanje_r==3 and isinstance(inverz, Matrika):
+    elif stanje_r == 3 and isinstance(inverz, Matrika):
         napaka = stanje.preveri_podatke_nove_matrike(inverz)
         if not napaka:
             stanje.dodaj_matriko(inverz)
             shrani_stanje_trenutnega_uporabnika(stanje)
-    if  stopnja!='p':
-        if stopnja=='n':
+    if stopnja != 'p':
+        if stopnja == 'n':
             stopnja = bottle.request.forms.getunicode("stopnja_potence")
         rezultat_potenciranja = matrika.potenciraj(stopnja)
-        if isinstance(rezultat_potenciranja, Matrika)==False:
+        if not isinstance(rezultat_potenciranja, Matrika):
             napake.update(rezultat_potenciranja)
         else:
-             if stanje_r==4:
-                napaka = stanje.preveri_podatke_nove_matrike(rezultat_potenciranja)
+            if stanje_r == 4:
+                napaka = stanje.preveri_podatke_nove_matrike(
+                    rezultat_potenciranja)
                 if not napaka:
                     stanje.dodaj_matriko(rezultat_potenciranja)
                     shrani_stanje_trenutnega_uporabnika(stanje)
     else:
-          rezultat_potenciranja=''
-    if skalar!='s':
-        if skalar=='n':
-           skalar = bottle.request.forms.getunicode("zeljen_skalar")
+        rezultat_potenciranja = ''
+    if skalar != 's':
+        if skalar == 'n':
+            skalar = bottle.request.forms.getunicode("zeljen_skalar")
         rezultat_mnozenjas = matrika.mnozenje_s_skalar(skalar)
-        if isinstance(rezultat_mnozenjas, Matrika)==False:
+        if not isinstance(rezultat_mnozenjas, Matrika):
             napake.update(rezultat_mnozenjas)
         else:
-           if stanje_r==5:
-               napaka = stanje.preveri_podatke_nove_matrike(rezultat_mnozenjas)
-               if not napaka:
-                stanje.dodaj_matriko(rezultat_mnozenjas)
-                shrani_stanje_trenutnega_uporabnika(stanje)
+            if stanje_r == 5:
+                napaka = stanje.preveri_podatke_nove_matrike(
+                    rezultat_mnozenjas)
+                if not napaka:
+                    stanje.dodaj_matriko(rezultat_mnozenjas)
+                    shrani_stanje_trenutnega_uporabnika(stanje)
     else:
-        rezultat_mnozenjas=""
-    return bottle.template("rezultat_ene.tpl", matrika_izbrana=matrika, transponirana=transponirana, prirejenka=prirejenka, 
-    inverz=inverz, determinanta=determinanta, sled=sled, skalar=skalar, stopnja=stopnja, rezultat_potenciranja=rezultat_potenciranja,
-    rezultat_mnozenjas=rezultat_mnozenjas,
-    id_matrike_izbrana=id_matrike, matrike=stanje.matrike, operacije=OPERACIJE, napake=napake)
+        rezultat_mnozenjas = ""
+    return bottle.template("rezultat_ene.tpl", matrika_izbrana=matrika, transponirana=transponirana, prirejenka=prirejenka,
+                           inverz=inverz, determinanta=determinanta, sled=sled, skalar=skalar, stopnja=stopnja, rezultat_potenciranja=rezultat_potenciranja,
+                           rezultat_mnozenjas=rezultat_mnozenjas,
+                           id_matrike_izbrana=id_matrike, matrike=stanje.matrike, operacije=OPERACIJE, napake=napake)
+
+
+@bottle.post("/operacija-dve/<stanje_r:int>/<id_matrike1>/<id_matrike2>/")
+def operacija_dve(stanje_r, id_matrike1, id_matrike2):
+    stanje = stanje_trenutnega_uporabnika()
+    if id_matrike1 == 'n' and id_matrike2 == 'n':
+        id_matrike1 = int(bottle.request.forms["matrika1"])
+        id_matrike2 = int(bottle.request.forms["matrika2"])
+    napake = {}
+    matrika1 = stanje.matrike[int(id_matrike1)]
+    matrika2 = stanje.matrike[int(id_matrike2)]
+    vsota = matrika1 + matrika2
+    if not isinstance(vsota, Matrika):
+        napake.update(vsota)
+    produkt = matrika1 * matrika2
+    if not isinstance(produkt, Matrika):
+        napake.update(produkt)
+    if stanje_r == 1 and isinstance(vsota, Matrika):
+        napaka = stanje.preveri_podatke_nove_matrike(vsota)
+        if not napaka:
+            stanje.dodaj_matriko(vsota)
+            shrani_stanje_trenutnega_uporabnika(stanje)
+    elif stanje_r == 2 and isinstance(produkt, Matrika):
+        napaka = stanje.preveri_podatke_nove_matrike(produkt)
+        if not napaka:
+            stanje.dodaj_matriko(produkt)
+            shrani_stanje_trenutnega_uporabnika(stanje)
+    return bottle.template("rezultat_dveh.tpl", matrika1=matrika1, matrika2=matrika2, vsota=vsota, produkt=produkt,
+                           id_matrike1=id_matrike1, id_matrike2=id_matrike2, matrike=stanje.matrike, operacije=OPERACIJE, napake=napake)
+
 
 @bottle.error(404)
 def error_404(error):
-    return "Ta stran ne obstaja!" 
-            
-bottle.run(debug=True, reloader=True)
+    return "Ta stran ne obstaja!"
+
+
+bottle.run(debug=True, host="localhost", port=8080, reloader=True)
